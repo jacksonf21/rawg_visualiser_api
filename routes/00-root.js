cebf7f5a2764bc9851418b1b4f2cd98d472dc5b1
jacksonf21 <jacksonf21@hotmail.com>
2019-10-26 11:49:27 -0400

    i graphql

--- routes/00-root.js
+++ routes/00-root.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  
   res.send('welcome');
 });
 
